46e55029ed27077f18102ab91038613b39235e73
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-10-27 20:03:09 +0530

    updated

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -1,7 +1,7 @@
 const config = {
     user: 'username',
     password: "ashutosh@123" ,
-    server:  '172.16.100.79',
+    server:  '1**.**.100.21',
     database: "WA_DB",
     options: {
         encrypt: false, // For secure connection
